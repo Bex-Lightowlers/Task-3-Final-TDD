5d15cd1417c62873860f9bde3836fcb7a1460226
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-15 21:26:38 +0100

    Passed test

--- test_leap_year.py
+++ test_leap_year.py
@@ -1,8 +1,12 @@
 import unittest
+from leap_year import is_leap_year
 
 class TestLeapYear(unittest.TestCase):
     def test_tests_work(self):
         self.assertTrue(2+2,4)
+
+    def test_true_leap_year(self):
+        self.assertTrue((2000)) #is a leap year
 
 if __name__=='__main__':
     unittest.main()
